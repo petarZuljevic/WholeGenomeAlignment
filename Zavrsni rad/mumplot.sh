14856f627c670ef486d756468bd466f19c600cb8
Petar Žuljević <petar.zulljevic@gmail.com>
2014-06-12 01:49:36 -0400

    Delete mumplot.sh

--- Zavrsni rad/mumplot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "set terminal png
-set output '$2'
-set key right bottom
-plot '$1' using 1:2 w l lt rgb 'green' title 'Forward', \
-'$1' using 3:4 w l lt rgb 'red' title 'Reverse'" | gnuplot
